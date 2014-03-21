dc226b7df83fe6648a0f3da9f729a6e3bdfca6a5
pierre <pierre@pierre-K53SV>
2014-03-21 18:14:00 +0100

    classpath relatif + suppression CORRECT du /bin + modif build.sh (plus de compilation)

--- build.sh
+++ build.sh
@@ -5,12 +5,13 @@ user=$(echo "$USER")
 
 if (( $catalina_count != 1 ))
 then
-	$CATALINA_HOME/bin/shutdown.sh	
+	$CATALINA_HOME/bin/shutdown.sh
+	sleep 1
 fi
-	javac -classpath . -d WEB-INF/classes/ src/bdd/modeles/*.java
-	javac -classpath .:./WEB-INF/lib/jus.util.jar:./WEB-INF/lib/ojdbc14.jar:./WEB-INF/classes/ -d WEB-INF/classes/ src/bdd/accessBD/*.java	
-	javac -classpath .:../../lib/servlet-api.jar:./WEB-INF/lib/bd.jar:./WEB-INF/classes/ -d WEB-INF/classes/ src/servlets/utils/*.java	
-	javac -classpath .:../../lib/servlet-api.jar:./WEB-INF/lib/bd.jar:./WEB-INF/classes/ -d WEB-INF/classes/ src/servlets/*.java
+	#javac -classpath . -d WEB-INF/classes/ src/bdd/modeles/*.java
+	#javac -classpath .:./WEB-INF/lib/jus.util.jar:./WEB-INF/lib/ojdbc14.jar:./WEB-INF/classes/ -d WEB-INF/classes/ src/bdd/accessBD/*.java	
+	#javac -classpath .:../../lib/servlet-api.jar:./WEB-INF/lib/bd.jar:./WEB-INF/classes/ -d WEB-INF/classes/ src/servlets/utils/*.java	
+	#javac -classpath .:../../lib/servlet-api.jar:./WEB-INF/lib/bd.jar:./WEB-INF/classes/ -d WEB-INF/classes/ src/servlets/*.java
 
 	$CATALINA_HOME/bin/startup.sh
 
