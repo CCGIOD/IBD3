47b7fe08daa0ad878b7000ebfe05ec0b27bedd90
pierre <pierre@pierre-K53SV>
2014-03-28 09:36:33 +0100

    ajout d'une servlet consultation caddie + tunnel2 (mandelbrot2)

---
+++ src/servlets/ConsultationCaddieServlet.java
@@ -0,0 +1,83 @@
+package servlets;
+
+/*
+ * @(#)NouvelleRepresentationServlet.java	1.0 2007/10/31
+ * 
+ * Copyright (c) 2007 Sara Bouchenak.
+ */
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+import java.io.IOException;
+
+/**
+ * NouvelleRepresentation Servlet.
+ *
+ * This servlet dynamically adds a new date a show.
+ *
+ * @author <a href="mailto:Sara.Bouchenak@imag.fr">Sara Bouchenak</a>
+ * @version 1.0, 31/10/2007
+ */
+
+@SuppressWarnings("serial")
+public class ConsultationCaddieServlet extends HttpServlet {
+
+	/**
+	 * HTTP GET request entry point.
+	 *
+	 * @param req	an HttpServletRequest object that contains the request 
+	 *			the client has made of the servlet
+	 * @param res	an HttpServletResponse object that contains the response 
+	 *			the servlet sends to the client
+	 *
+	 * @throws ServletException   if the request for the GET could not be handled
+	 * @throws IOException	 if an input or output error is detected 
+	 *				 when the servlet handles the GET request
+	 */
+	public void doGet(HttpServletRequest req, HttpServletResponse res)
+			throws ServletException, IOException
+			{
+		ServletOutputStream out = res.getOutputStream();   
+
+		res.setContentType("text/html");
+
+		out.println("<HEAD><TITLE> Consultation du caddie </TITLE><LINK rel=\"stylesheet\" type=\"text/css\" href=\"../style.css\"></HEAD>");
+		out.println("<BODY>");
+		out.println("<h1> Consultation du caddie : </h1>");
+
+		out.println("<hr><p><a href=\"/index.html\">Page d'accueil</a></p>");
+		out.println("</BODY>");
+		out.close();
+
+			}
+
+	/**
+	 * HTTP POST request entry point.
+	 *
+	 * @param req	an HttpServletRequest object that contains the request 
+	 *			the client has made of the servlet
+	 * @param res	an HttpServletResponse object that contains the response 
+	 *			the servlet sends to the client
+	 *
+	 * @throws ServletException   if the request for the POST could not be handled
+	 * @throws IOException	   if an input or output error is detected 
+	 *					   when the servlet handles the POST request
+	 */
+	public void doPost(HttpServletRequest req, HttpServletResponse res)
+			throws ServletException, IOException
+			{
+		doGet(req, res);
+			}
+
+
+	/**
+	 * Returns information about this servlet.
+	 *
+	 * @return String information about this servlet
+	 */
+
+	public String getServletInfo() {
+		return "Ajoute une représentation à une date donnée pour un spectacle existant";
+	}
+
+}
---
+++ tunnel2.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+ssh -L1521:im2ag-oracle.e.ujf-grenoble.fr:1521 odinpi@mandelbrot2.e.ujf-grenoble.fr
